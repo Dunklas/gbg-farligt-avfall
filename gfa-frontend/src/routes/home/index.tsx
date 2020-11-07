5e1a69dc7b9946a785de4add5b927d246774ef93
Rickard Andersson <dunklas@gmail.com>
2020-11-07 11:19:54 +0100

    Fix some more lint issues

--- gfa-frontend/src/routes/home/index.tsx
+++ gfa-frontend/src/routes/home/index.tsx
@@ -1,33 +1,37 @@
-import { FunctionalComponent, h } from "preact";
+import { FunctionalComponent, h } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
 import { Stop } from '../../types/Stop';
 import { StopComponent } from '../../components/stop';
 import { ApiClient } from '../../api/apiClient';
-import * as style from "./style.css";
+import * as style from './style.css';
 
 const Home: FunctionalComponent = () => {
   const [stops, setStops] = useState<Stop[]>([]);
   useEffect(() => {
     const apiClient = new ApiClient(API_URL[0]);
-    apiClient.getStops()
+    apiClient
+      .getStops()
       .then(stops => {
         setStops(stops);
       })
       .catch(err => {
         console.log('I am error');
         console.error(err);
-      })
+      });
   }, []);
-
-  console.log(stops);
 
   return (
     <div className={style.home}>
       <div>
-        {stops.map(stop => <StopComponent stop={stop} key={stop.location_id} />)}
+        {stops.map(stop => (
+          <StopComponent stop={stop} key={stop.location_id} />
+        ))}
       </div>
       <div>
-        <p>Coordinates for each location are powered by <a href="http://www.mapquest.com">MapQuest</a></p>
+        <p>
+          Coordinates for each location are powered by
+          <a href="http://www.mapquest.com">MapQuest</a>
+        </p>
       </div>
     </div>
   );
