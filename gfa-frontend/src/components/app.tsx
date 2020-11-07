f495e8c8714127349563d4d3f3065ad38b617a52
Rickard Andersson <dunklas@gmail.com>
2020-11-07 11:02:00 +0100

    Fix lint issues in app.tsx

--- gfa-frontend/src/components/app.tsx
+++ gfa-frontend/src/components/app.tsx
@@ -1,21 +1,15 @@
-import { FunctionalComponent, h } from "preact";
-import { Route, Router, RouterOnChangeArgs } from "preact-router";
-
-import Home from "../routes/home";
+import { FunctionalComponent, h } from 'preact';
+import { Route, Router } from 'preact-router';
+import Home from '../routes/home';
 
 const App: FunctionalComponent<{}> = () => {
-    let currentUrl: string;
-    const handleRoute = (e: RouterOnChangeArgs) => {
-        currentUrl = e.url;
-    };
-
-    return (
-        <div id="app">
-            <Router onChange={handleRoute}>
-                <Route path="/" default component={Home} />
-            </Router>
-        </div>
-    );
+  return (
+    <div id="app">
+      <Router>
+        <Route path="/" default component={Home} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
