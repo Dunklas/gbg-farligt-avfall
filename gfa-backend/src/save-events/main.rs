61ccab927401c06b7059af154758066dd4cab6ea
Rickard Andersson <dunklas@gmail.com>
2020-11-10 23:27:13 +0100

    Print how many items that was saved

--- gfa-backend/src/save-events/main.rs
+++ gfa-backend/src/save-events/main.rs
@@ -2,7 +2,7 @@ use std::{env, str::FromStr};
 use lambda::{handler_fn, Context};
 use serde_json::{json, Value};
 use simple_logger::{SimpleLogger};
-use log::{self, error, LevelFilter};
+use log::{self, info, error, LevelFilter};
 use rusoto_core::Region;
 use chrono::{Utc};
 use common::pickup_event::PickUpEvent;
@@ -28,11 +28,15 @@ async fn handle_request(event: Value, _: Context) -> Result<Value, Error> {
     let region = Region::from_str(&region).unwrap(); 
     let pickup_events: Vec<PickUpEvent> = serde_json::from_value(event)?;
     let pickup_events = events_filter::filter(pickup_events, Utc::today());
+    let total_events = pickup_events.len();
     // Filter events
     // Skip in the past, long time in the future, etc
 
     let _result = match events_repo::store(events_table, region, pickup_events).await {
-        Ok(res) => res,
+        Ok(unprocessed_count) => {
+            let processed_items = total_events - unprocessed_count;
+            info!("Wrote {} / {} items", processed_items, total_events);
+        },
         Err(e) => {
             error!("Error when writing events:\n {}", e);
             return Err(e);
