9b66b0c1aa055e39085e0172ab488c40d3b24099
Rickard Andersson <dunklas@gmail.com>
2020-11-26 23:12:30 +0100

    Basic modal for subscription

--- gfa-frontend/src/routes/list/index.tsx
+++ gfa-frontend/src/routes/list/index.tsx
@@ -1,17 +1,37 @@
 import { FunctionalComponent, h } from 'preact';
+import { Stop } from '../../types/Stop';
 import { StopListItem } from '../../components/stop-list-item';
 import { StopsContext } from '../../components/app';
-import { Stop } from '../../types/Stop';
+import { Modal } from '../../components/modal';
 import * as style from './style.css';
-import { useContext } from 'preact/hooks';
+import { useContext, useState } from 'preact/hooks';
 
 const List: FunctionalComponent<{}> = () => {
+
   const stops = useContext(StopsContext);
+
+  const [showSubscribe, setShowSubscribe] = useState<boolean>(false);
+  const [stopToSubscribeTo, setStopToSubscribeTo] = useState<Stop | null>(null);
+
+  const onSubscribe = (stop: Stop): void => {
+    setStopToSubscribeTo(stop);
+    setShowSubscribe(true);
+  }
+
+  const onCloseSubscribeModal = (): void => {
+    setShowSubscribe(false);
+    setStopToSubscribeTo(null);
+  }
+
   return (
     <div className={style.main}>
       <div>
         {stops.map(stop => (
-          <StopListItem stop={stop} key={stop.location_id} />
+          <StopListItem
+            stop={stop}
+            key={stop.location_id}
+            onSubscribe={onSubscribe}
+          />
         ))}
       </div>
       <div>
@@ -20,6 +40,19 @@ const List: FunctionalComponent<{}> = () => {
           <a href="http://www.mapquest.com">MapQuest</a>
         </p>
       </div>
+      <Modal isOpen={showSubscribe} onClickBackdrop={onCloseSubscribeModal}>
+        {stopToSubscribeTo && <div className={style.modal}>
+          <p>You&apos;re about to subscribe to e-mail notifications for <strong>{stopToSubscribeTo.street}</strong>.</p>
+          <p>By subscribing you consent to that we will store your e-mail address and yada yada</p>
+          <form className={style.form}>
+            <label for="email">
+              E-mail
+              <input id="email" type="email" name="email" required />
+            </label>
+            <button>Subscribe!</button>
+          </form>
+        </div>}
+      </Modal>
     </div>
   );
 };
