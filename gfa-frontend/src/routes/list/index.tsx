8404f4660b966dc0c0e86db162796d75558152f1
Rickard Andersson <dunklas@gmail.com>
2021-05-03 23:28:22 +0200

    Start rebuild frontend

--- gfa-frontend/src/routes/list/index.tsx
+++ gfa-frontend/src/routes/list/index.tsx
@@ -1,44 +1,19 @@
 import { FunctionalComponent, h } from 'preact';
-import { Stop } from '../../types/Stop';
 import { StopListItem } from '../../components/stop-list-item';
 import { StopsContext } from '../../components/app';
-import { SubscribeModal } from '../../components/subscribe-modal';
 import * as style from './style.css';
-import { useContext, useState } from 'preact/hooks';
+import { useContext } from 'preact/hooks';
 
 const List: FunctionalComponent<{}> = () => {
   const stops = useContext(StopsContext);
-
-  const [showSubscribe, setShowSubscribe] = useState<boolean>(false);
-  const [stopToSubscribeTo, setStopToSubscribeTo] = useState<Stop | null>(null);
-
-  const onSubscribe = (stop: Stop): void => {
-    setStopToSubscribeTo(stop);
-    setShowSubscribe(true);
-  };
-
-  const onCloseSubscribeModal = (): void => {
-    setShowSubscribe(false);
-    setStopToSubscribeTo(null);
-  };
 
   return (
     <div className={style.main}>
       <div>
         {stops.map(stop => (
-          <StopListItem
-            stop={stop}
-            key={stop.location_id}
-            onSubscribe={onSubscribe}
-          />
+          <StopListItem stop={stop} key={stop.location_id} />
         ))}
       </div>
-      {showSubscribe && stopToSubscribeTo && (
-        <SubscribeModal
-          stop={stopToSubscribeTo}
-          onClose={onCloseSubscribeModal}
-        />
-      )}
     </div>
   );
 };
