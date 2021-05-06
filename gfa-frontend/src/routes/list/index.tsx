79bbca798299e70db2624a4c10c6f625f4a305d3
Rickard Andersson <dunklas@gmail.com>
2021-05-06 21:06:29 +0200

    More consistent spacing

--- gfa-frontend/src/routes/list/index.tsx
+++ gfa-frontend/src/routes/list/index.tsx
@@ -13,13 +13,11 @@ const List: FunctionalComponent<{}> = () => {
         <h1>Greetings!</h1>
         <p>
           Here you can subscribe to e-mail notifications when the Göteborg
-          Farligt Avfall truck is about to arrive.
+          Farligt Avfall truck is about to arrive. Note that the notifications
+          are provided as a best effort and that we are in no way affiliated
+          with Göteborg stad.
         </p>
         <p>Click on one of the locations below to subscribe.</p>
-        <p>
-          Note that the notifications are provided as a best effort, and that we
-          are in no way affiliated with Göteborg stad.
-        </p>
       </div>
       <div className={style.list}>
         {stops.map(stop => (
